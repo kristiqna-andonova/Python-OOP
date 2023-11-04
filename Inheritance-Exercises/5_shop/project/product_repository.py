7e83089076bf3cc0ca31fc463ef61fed5e32530a
Kristiqna <123325312+kristiqna-andonova@users.noreply.github.com>
2023-11-04 10:35:01 +0200

    taks

---
+++ Inheritance-Exercises/5_shop/project/product_repository.py
@@ -0,0 +1,22 @@
+from project.product import Product
+
+
+class ProductRepository:
+    def __init__(self):
+        self.products = []
+
+    def add(self, product: Product):
+        self.products.append(product)
+
+    def find(self, product_name: str):
+        for p in self.products:
+            if p.name == product_name:
+                return p
+
+    def remove(self, product_name):
+        product = self.find(product_name)
+        if product:
+            self.products.remove(product)
+
+    def __repr__(self):
+        return "\n".join([f"{p.name}: {p.quantity}" for p in self.products])
