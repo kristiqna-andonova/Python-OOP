bca4f2b8ac89681e839cdc25b5d06eaab563916b
Kristiqna <123325312+kristiqna-andonova@users.noreply.github.com>
2023-11-08 10:43:25 +0200

    tasks

---
+++ Encapsulation-Lab/4_email_validator.py
@@ -0,0 +1,29 @@
+class EmailValidator:
+    def __init__(self, min_length, mails: list, domains: list):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def __is_name_valid(self, name):
+        return self.min_length <= len(name)
+
+    def __is_mail_valid(self, mail):
+        return mail in self.mails
+
+    def __is_domain_valid(self, domain):
+        return domain in self.domains
+
+    def validate(self, email):
+        name, data = email.split("@")
+        mail, domain = data.split(".")
+        return (self.__is_domain_valid(domain) and self.__is_mail_valid(mail) and self.__is_name_valid(name))
+
+
+mails = ["gmail", "softuni"]
+domains = ["com", "bg"]
+email_validator = EmailValidator(6, mails, domains)
+print(email_validator.validate("pe77er@gmail.com"))
+print(email_validator.validate("georgios@gmail.net"))
+print(email_validator.validate("stamatito@abv.net"))
+print(email_validator.validate("abv@softuni.bg"))
+
